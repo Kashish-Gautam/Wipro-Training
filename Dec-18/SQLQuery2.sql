d4eaa50c5c76cb60805c471f69f6d38ac46960bb
Kashish Gautam <gautamkashish1311@gmail.com>
2025-12-18 20:51:34 +0530

    Day 6 training file2

---
+++ Dec-18/SQLQuery2.sql
@@ -0,0 +1,5 @@
+INSERT INTO Students (FirstName , LastName , Email, DateOfBirth)
+VALUEs
+('Amit' , 'Sharma' , 'amit@uni.com' , '2001-05-10'),
+('Neha' , 'Verma' , 'neha@uni.com' , '2000-08-21'),
+('Rahul' , 'Mehta' , 'rahul@uni.com' , '1999-12-15');
